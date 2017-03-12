e64de107e8c83bc74969528a54079b84125dafe4
jsap1904 <sapinoso.j@gmail.com>
2017-03-12 15:32:36 -0700

    upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// for(var i =0; i<=100; i+=5)
+// 	console.log([i]);
+
+// function getInfo() {
+
+// var username = document.getElementById("username").value
+// var password = document.getElementById("password").value
+// console.log("you're username is "+ username + " and your password is " + password)
+// }
