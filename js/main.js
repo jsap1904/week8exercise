f1d89f7a62ce05a2cb58cf08c7b5c61ec1cb2b02
jsap1904 <sapinoso.j@gmail.com>
2017-03-14 19:30:35 -0700

    update

--- js/main.js
+++ js/main.js
@@ -8,30 +8,30 @@
 // console.log("you're username is "+ username + " and your password is " + password)
 // }
 
-var houseObject = []
-houseObject[0] = {
-	location: "sd",
-	stories: "two",
-	age: 1990,
-	house1: new Image(),
-	src: "img/house1.jpg"
-},
-houseObject[1] = {
-	location: "sf",
-	stories: "two",
-	age: 1995 ,
-	house1: new Image(),
-	src: "img/house2.jpg"
-},
-houseObject[2] = {
-	location: "la",
-	stories: "two",
-	age: 2000,
-	house2: new Image(),
-	src: "img/house3.jpg"
-}
+// var houseObject = []
+// houseObject[0] = {
+// 	location: "sd",
+// 	stories: "two",
+// 	age: 1990,
+// 	house1: new Image(),
+// 	src: "img/house1.jpg"
+// },
+// houseObject[1] = {
+// 	location: "sf",
+// 	stories: "two",
+// 	age: 1995 ,
+// 	house1: new Image(),
+// 	src: "img/house2.jpg"
+// },
+// houseObject[2] = {
+// 	location: "la",
+// 	stories: "two",
+// 	age: 2000,
+// 	house2: new Image(),
+// 	src: "img/house3.jpg"
+// }
 
-for(i=0; i < houseObject.length; i++) {
+// for(i=0; i < houseObject.length; i++) {
 	
-document.write("<img src=" + houseObject[i].src  + " width='160' height='120'>" + houseObject[i].location + "," + houseObject[i].stories + "," + houseObject[i].age );	
-}
+// document.write("<img src=" + houseObject[i].src  + " width='160' height='120'>" + houseObject[i].location + "," + houseObject[i].stories + "," + houseObject[i].age );	
+// }
